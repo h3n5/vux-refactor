57235affca29e7417a569b464106ca4f192e5697
ice-orange <295779204@qq.com>
2024-10-16 10:56:23 +0800

    feat: add github page

---
+++ vite.doc.config.js
@@ -0,0 +1,32 @@
+// vite.config.js
+import vue from '@vitejs/plugin-vue2'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import path from 'path'
+/** @type {import('vite').UserConfig} */
+export default {
+  base: '/vux-refactor',
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  },
+  server: {
+    host: true,
+    port: 5174,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'vux-refactor': path.resolve(__dirname, './dist/vux-refactor.es.js'),
+    },
+  },
+  build: {
+    outDir: 'docs',
+  },
+  plugins: [
+    vue(),
+    VueI18nPlugin({
+      defaultSFCLang: 'yaml',
+      include: [path.resolve(__dirname, './src/components/**/*.vue')],
+      runtimeOnly: false,
+    }),
+  ],
+}
